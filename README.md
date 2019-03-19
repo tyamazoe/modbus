01b6974fa195d7e64392ed18e95b414c940a42f5
tyamazoe <tomohisa.yamazoe@gmail.com>
2019-03-19 16:44:11 +0900

    [add] Added modbus_reader
    Added reader/modbus_reader.py. Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # modbus
 Modbus utility programs
+
+## reader/modbus_reader.py
+Read modbus register by modbus/tcp. Support only function code 0x03 and 0x04.
+
+### Usage
+
+`$python modbus_reader.py [ip address]`
+
+### Example
+Normal
+```
+$python modbus_reader.py 192.168.1.101
+
+Enter Modbus Params
+ Unit Identifier (Hex): 2
+ Function Code (Hex): 4
+ Start Register (Hex) : 290
+ Num of Registers (Hex) : 4
+
+TX: b'000000000006020402900004'
+RX: b'00000000000b02040803f2020a020a0000'
+
+Modbus Application Data Unit (ADU)
+ Transaction Identifier : 0
+ Protocol Identifier : 0
+ Length : 11
+ Unit Identifier : 2
+ Function Code : 4
+ Byte Count : 8
+
+Register: Value
+ #0x290 : 0x03f2 : 1010
+ #0x291 : 0x020a : 522
+ #0x292 : 0x020a : 522
+ #0x293 : 0x0000 : 0
+Done
+```
+Error
+```
+$python modbus_reader.py 192.168.1.101
+Enter Modbus Params
+ Unit Identifier (Hex): 4
+ Function Code (Hex): 4
+ Start Register (Hex) : 290
+ Num of Registers (Hex) : 10
+
+TX: b'000000000006040402900010'
+RX: b'00000000000304840a'
+
+Modbus Application Data Unit (ADU)
+ Transaction Identifier : 0
+ Protocol Identifier : 0
+ Length : 3
+ Unit Identifier : 4
+ Error Code     : 0x84 : 132
+ Exception Code : 0x0a : 10
+Done
+```
+
+
---
+++ reader/modbus_reader.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#
+# Modbus/TCP reader
+#
+import socket
+import struct
+import time
+import traceback
+import codecs
+import sys
+
+TARGET_IP = '192.168.1.99'
+try:
+    TARGET_IP = sys.argv[1]
+except IndexError:
+    pass # use defaults
+
+TARGET_PORT = 502
+buffer_size = 0
+SUPPORTED_FC = (0x03, 0x04)
+try:
+    print("\nEnter Modbus Params")
+    unitId = int(input(" Unit Identifier (Hex): "), 16)
+    functionCode = int(input(" Function Code (Hex): "), 16)
+    if functionCode not in  SUPPORTED_FC:
+        raise Exception("FC only support {}".format(SUPPORTED_FC))
+    startRegister = int(input(" Start Register (Hex) : "), 16)
+    numRegister = int(input(" Num of Registers (Hex) : "), 16)
+
+    # Send request
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((TARGET_IP, TARGET_PORT))
+    req = struct.pack('>3H 2B 2H', 0, 0, 6, unitId, functionCode, startRegister, numRegister)
+    sock.send(req)
+
+    # Receive response
+    buffer_size = 3*2 + 3 + numRegister*2
+    res = sock.recv(buffer_size)
+    print("\nTX: {0}".format(codecs.encode(req, 'hex_codec')))
+    print("RX: {0}".format(codecs.encode(res, 'hex_codec')))
+
+    if len(res) == buffer_size:
+        # normal response
+        s = struct.Struct('>3H 3B %sH' %(numRegister))
+    else:
+        # error response
+        s = struct.Struct('>3H 3B')
+    data = s.unpack(res)
+
+    print("\nModbus Application Data Unit (ADU)")
+    print(" Transaction Identifier : %s" %data[0])
+    print(" Protocol Identifier : %s" %data[1])
+    print(" Length : %s" %data[2])
+    print(" Unit Identifier : %s" %data[3])
+    if len(data) > 6:
+        print(" Function Code : %s" %data[4])
+        print(" Byte Count : %s" %data[5])
+        print("\nRegister: Value")
+        for i in range(6, 6+numRegister):
+            currentRegister = i-6 + startRegister
+            print("  #0x{0:x} : 0x{1:04x} : {1}".format(currentRegister, data[i]))
+    else:
+        print(" Error Code     : 0x{0:02x} : {0}".format(data[4]))
+        print(" Exception Code : 0x{0:02x} : {0}".format(data[5]))
+
+    sock.close()
+except:
+    print(traceback.format_exc())
+finally:
+    print("\nDone")
